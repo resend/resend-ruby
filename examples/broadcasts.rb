03f3115d5494a5591f87d12b1ba02c6bc355d7ce
Derich Pacheco <carlosderich@gmail.com>
2024-12-22 23:34:46 -0300

    feat: Implement broadcasts (#83)

---
+++ examples/broadcasts.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative "../lib/resend"
+
+raise if ENV["RESEND_API_KEY"].nil?
+
+Resend.api_key = ENV["RESEND_API_KEY"]
+
+params = {
+  from: "onboarding@resend.dev",
+  subject: "Hello",
+  audience_id: "78b8d3bc-a55a-45a3-aee6-6ec0a5e13d7e", # replace with an existing audience id
+  text: "Hello, how are you?",
+}
+
+broadcast = Resend::Broadcasts.create(params)
+puts "created broadcast: #{broadcast[:id]}"
+
+send_params = {
+  broadcast_id: broadcast[:id],
+  scheduled_at: "in 1 min",
+}
+
+sent_broadcast = Resend::Broadcasts.send(send_params)
+puts "sent broadcast: #{sent_broadcast[:id]}"
+
+broadcasts = Resend::Broadcasts.list
+puts broadcasts
+
+retrieved = Resend::Broadcasts.get(broadcast[:id])
+puts "retrieved #{retrieved[:id]}"
+
+Resend::Broadcasts.remove(broadcast[:id])
+puts "removed #{broadcast[:id]}"
---
+++ lib/resend/broadcasts.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "resend/request"
+require "resend/errors"
+
+module Resend
+  # broadcasts api wrapper
+  module Broadcasts
+    class << self
+      # https://resend.com/docs/api-reference/broadcasts/create-broadcast
+      def create(params = {})
+        path = "broadcasts"
+        Resend::Request.new(path, params, "post").perform
+      end
+
+      # https://resend.com/docs/api-reference/broadcasts/send-broadcast
+      def send(params = {})
+        path = "broadcasts/#{params[:broadcast_id]}/send"
+        Resend::Request.new(path, params, "post").perform
+      end
+
+      # https://resend.com/docs/api-reference/broadcasts/list-broadcasts
+      def list
+        path = "broadcasts"
+        Resend::Request.new(path, {}, "get").perform
+      end
+
+      # https://resend.com/docs/api-reference/broadcasts/delete-broadcast
+      def remove(broadcast_id = "")
+        path = "broadcasts/#{broadcast_id}"
+        Resend::Request.new(path, {}, "delete").perform
+      end
+
+      # https://resend.com/docs/api-reference/broadcasts/get-broadcast
+      def get(broadcast_id = "")
+        path = "broadcasts/#{broadcast_id}"
+        Resend::Request.new(path, {}, "get").perform
+      end
+    end
+  end
+end
