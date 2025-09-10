ce41811a29f35bf2d757250f49623a655c18913c
Derich Pacheco <carlosderich@gmail.com>
2025-09-09 21:40:29 -0300

    lint: fix

--- lib/resend/emails.rb
+++ lib/resend/emails.rb
@@ -34,20 +34,20 @@ module Resend
 
       # List emails with optional pagination.
       # see more: https://resend.com/docs/api-reference/emails/list-emails
-      # 
+      #
       # @param options [Hash] Optional parameters for pagination
       # @option options [Integer] :limit Maximum number of emails to return (1-100, default 20)
       # @option options [String] :after Cursor for pagination (newer emails)
       # @option options [String] :before Cursor for pagination (older emails)
       def list(options = {})
         path = "emails"
-        
+
         # Build query parameters, filtering out nil values
         query_params = {}
         query_params[:limit] = options[:limit] if options[:limit]
         query_params[:after] = options[:after] if options[:after]
         query_params[:before] = options[:before] if options[:before]
-        
+
         Resend::Request.new(path, query_params, "get").perform
       end
     end
