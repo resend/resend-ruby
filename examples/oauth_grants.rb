b0466e1d136fd1783e7a977cef8172c42506fe93
Felipe Freitag Vargas <ffvargas@gmail.com>
2026-07-09 15:42:00 -0300

    feat: add OAuth grants support (#197)

---
+++ examples/oauth_grants.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative "../lib/resend"
+
+raise if ENV["RESEND_API_KEY"].nil?
+
+Resend.api_key = ENV["RESEND_API_KEY"]
+
+def list
+  grants = Resend::OAuthGrants.list
+  puts grants
+end
+
+def list_paginated
+  paginated_grants = Resend::OAuthGrants.list({ limit: 10, after: "grant_id_here" })
+  puts "Paginated response:"
+  puts paginated_grants
+  puts "Has more: #{paginated_grants[:has_more]}" if paginated_grants[:has_more]
+end
+
+def revoke
+  grant = Resend::OAuthGrants.revoke("oauth_grant_id_here")
+  puts grant
+end
+
+list
+# list_paginated
+# revoke
---
+++ lib/resend/oauth_grants.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Resend
+  # oauth grants api wrapper
+  module OAuthGrants
+    class << self
+      # https://resend.com/docs/api-reference/oauth/list-grants
+      def list(params = {})
+        path = Resend::PaginationHelper.build_paginated_path("oauth/grants", params)
+        Resend::Request.new(path, {}, "get").perform
+      end
+
+      # https://resend.com/docs/api-reference/oauth/revoke-grant
+      def revoke(oauth_grant_id = "")
+        path = "oauth/grants/#{oauth_grant_id}"
+        Resend::Request.new(path, {}, "delete").perform
+      end
+    end
+  end
+end
